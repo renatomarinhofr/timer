b8ef1d9c3013b6345c8d6823a0f4a19c0d40e796
Renato Marinho <renato.marinhofr@gmail.com>
2022-07-31 00:40:22 -0300

    feat: define layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
-import { History } from './pages/history';
+import { DefaultLayout } from './layouts/default-layout';
 
+import { History } from './pages/history';
 import { Home } from './pages/home';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   );
 }

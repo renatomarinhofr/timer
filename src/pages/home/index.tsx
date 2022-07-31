bff342caab3be7e1f37227cd51affd664063a521
Renato Marinho <renato.marinhofr@gmail.com>
2022-07-31 01:04:56 -0300

    feat: basic home page structure

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { Play } from 'phosphor-react';
+
+import * as S from './styles';
+
+export function Home() {
+  return (
+    <S.HomeContainer>
+      <form>
+        <S.FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <S.TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <S.MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </S.FormContainer>
+
+        <S.CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.CountdownContainer>
+
+        <S.StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </S.StartCountdownButton>
+      </form>
+    </S.HomeContainer>
+  );
+}
